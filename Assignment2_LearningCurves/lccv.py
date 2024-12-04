8e193135f26cb66968b788c21f6ec52a1debaf2e
CDemmenie <11903978+Demmenie@users.noreply.github.com>
2024-12-04 15:38:05 +0100

    First pass at lccv optimistic_extrapolation

---
+++ Assignment2_LearningCurves/lccv.py
@@ -0,0 +1,60 @@
+import logging
+import numpy as np
+import typing
+
+from vertical_model_evaluator import VerticalModelEvaluator
+
+class LCCV(VerticalModelEvaluator):
+    
+    @staticmethod
+    def optimistic_extrapolation(
+        previous_anchor: int, previous_performance: float, 
+        current_anchor: int, current_performance: float, target_anchor: int
+    ) -> float:
+        """
+        Does the optimistic performance. Since we are working with a simplified
+        surrogate model, we can not measure the infimum and supremum of the
+        distribution. Just calculate the slope between the points, and
+        extrapolate this.
+
+        :param previous_anchor: See name
+        :param previous_performance: Performance at previous anchor
+        :param current_anchor: See name
+        :param current_performance: Performance at current anchor
+        :param target_anchor: the anchor at which we want to have the
+        optimistic extrapolation
+        :return: The optimistic extrapolation of the performance
+        """
+        
+        mean = (current_performance + previous_performance) / 2
+        sd = ((current_performance - mean)**2 + (previous_performance - mean)**2) / 2
+
+        C_i = (np.pi * sd) * np.exp(-0.5*((-mean)/sd)**2)
+
+        supC_t1 = previous_performance + C_i
+        infC_t = current_performance - C_i
+
+        sub = ((supC_t1 - infC_t) / (previous_anchor - current_anchor))
+        optPerf = infC_t - (target_anchor - current_anchor) * sub
+
+        return optPerf
+    
+
+    def evaluate_model(self, best_so_far: typing.Optional[float], configuration: typing.Dict) -> typing.List[typing.Tuple[int, float]]:
+        """
+        Does a staged evaluation of the model, on increasing anchor sizes.
+        Determines after the evaluation at every anchor an optimistic
+        extrapolation. In case the optimistic extrapolation can not improve
+        over the best so far, it stops the evaluation.
+        In case the best so far is not determined (None), it evaluates
+        immediately on the final anchor (determined by self.final_anchor)
+
+        :param best_so_far: indicates which performance has been obtained so far
+        :param configuration: A dictionary indicating the configuration
+
+        :return: A tuple of the evaluations that have been done. Each element of
+        the tuple consists of two elements: the anchor size and the estimated
+        performance.
+        """
+
+        raise NotImplemented()
